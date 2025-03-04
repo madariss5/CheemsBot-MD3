1961a9f5bcfa5d15667649dde6a5a5341b9a4516
madariss5 <menyhartmartin4@gmail.com>
2025-03-04 18:15:02 +0100

    Update config.js

--- config.js
+++ config.js
@@ -41,7 +41,7 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['4915563151347'] //ur owner number
+global.owner = ['4915562378343'] //ur owner number
 global.ownername = "Martin"
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
@@ -55,8 +55,8 @@ global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n🦄🦄🦄 "
 global.author = "🦄Dream Guy Xeon"
 global.themeemoji = "🐶"
 global.reactmoji = "😘"
-global.ownertag = ['4915563151347'] //ur owner tag
-global.ownernummenu = ['4915563151347'] //ur owner number in menu and all
+global.ownertag = ['4915562378343'] //ur owner tag
+global.ownernummenu = ['4915562378343'] //ur owner number in menu and all
 global.watermark = "Xeon Bot Inc." //ur watermark
 global.botscript = "https://github.com/DGXeon/CheemsBot-MD3"
 global.linkz1 = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS"
